76519a4fa4d1eca0c5a349c54ea5c859db3c0d82
TomLeroyer <tommy.leroyer@icloud.com>
2024-02-21 15:04:31 +0100

    Mini jeu 1
    avancée sur le mini jeu

--- Assets/Scripts/Character/Obstacle.cs
+++ Assets/Scripts/Character/Obstacle.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
-public class test1 : MonoBehaviour
+public class Obstacle : MonoBehaviour
 {
-    public float interactionRadius = 2f; // Rayon de proximité pour interagir avec l'objet
     public KeyCode interactionKey = KeyCode.E; // Touche pour l'interaction
     private bool isInRange = false;
+    public GameObject cube; // Référence au cube à faire apparaître/disparaître
+    public int nbPressionBouton = 0;
 
     private void Update()
     {
+        // Vérifie si le joueur est dans la zone d'interaction et appuie sur la touche d'interaction
         if (isInRange && Input.GetKeyDown(interactionKey))
         {
-            Debug.Log("GIROUD NTM");
+            nbPressionBouton ++;
+            if (nbPressionBouton == 4)
+            {
+                Debug.Log("Omg y a Bonnie");
+            }
+              
+            // Lance la coroutine pour faire réapparaître et disparaître le cube
+            StartCoroutine(ShowAndHideCubeRepeatedly());
+        }
+    }
+
+    IEnumerator ShowAndHideCubeRepeatedly()
+    {
+        int numRepeats = 10; // Nombre de répétitions (à ajuster selon vos besoins)
+        float interval = 0.1f; // Intervalle entre chaque répétition (en secondes)
+        
+        for (int i = 0; i < numRepeats; i++)
+        {
+            // Active le cube
+            cube.SetActive(true);
+
+            // Attend un court instant
+            yield return new WaitForSeconds(interval);
+
+            // Désactive le cube
+            cube.SetActive(false);
+
+            // Attend un court instant
+            yield return new WaitForSeconds(interval);
+
+            // Réactive le cube à la fin de la séquence de répétitions
+        cube.SetActive(true);
         }
     }
 
@@ -30,4 +64,3 @@ public class test1 : MonoBehaviour
         }
     }
 }
-
