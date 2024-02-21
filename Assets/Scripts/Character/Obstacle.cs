f230a4ed5bb2ee04922578f0e2b4ae353998d225
Nasserddine <nasserddinedkair7@gmail.com>
2024-02-21 15:46:30 +0100

    test

--- Assets/Scripts/Character/Obstacle.cs
+++ Assets/Scripts/Character/Obstacle.cs
@@ -1,28 +1,20 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-=======
-using UnityEngine;
->>>>>>> parent of 76519a4 (Mini jeu 1)
 
 public class test1 : MonoBehaviour
 {
     public float interactionRadius = 2f; // Rayon de proximité pour interagir avec l'objet
     public KeyCode interactionKey = KeyCode.E; // Touche pour l'interaction
     private bool isInRange = false;
-<<<<<<< HEAD
 
     public int nbItems = 0; //nb d'items
-=======
->>>>>>> parent of 76519a4 (Mini jeu 1)
 
     private void Update()
     {
         if (isInRange && Input.GetKeyDown(interactionKey))
         {
-<<<<<<< HEAD
             if (gameObject.CompareTag("item")){
                 nbItems += 1; 
                 Debug.Log(nbItems);
@@ -31,9 +23,6 @@ public class test1 : MonoBehaviour
             if (nbItems == 1) {
                 SceneManager.LoadScene("gameOver");
             }
-=======
-            Debug.Log("GIROUD NTM");
->>>>>>> parent of 76519a4 (Mini jeu 1)
         }
     }
 
